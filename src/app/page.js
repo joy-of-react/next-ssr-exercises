585b27fbd22ad51bdf417e8a919dfd179e42f8d5
Joshua Comeau <joshwcomeau@gmail.com>
2023-05-12 14:35:02 -0400

    Initial code

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import Counter from '../components/Counter';
+
 function Home() {
-  return <p>Hello world!</p>;
+  return <Counter />;
 }
 
 export default Home;
---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,22 @@
+'use client';
+import React from 'react';
+
+function Counter() {
+  const [count, setCount] = React.useState(() => {
+    return Number(
+      window.localStorage.getItem('saved-count') || 0
+    );
+  });
+
+  React.useEffect(() => {
+    window.localStorage.setItem('saved-count', count);
+  }, [count]);
+
+  return (
+    <button onClick={() => setCount(count + 1)}>
+      Count: {count}
+    </button>
+  );
+}
+
+export default Counter;
