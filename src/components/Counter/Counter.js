407c0a05ee54ebaed5ccb2f3c77a9fa69669018a
Andres Sepulveda <andresdsep@yahoo.es>
2023-07-24 04:19:18 +0000

    Fix server side error

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,22 +1,19 @@
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
 
 function Counter() {
-  const [count, setCount] = React.useState(() => {
-    return Number(
-      window.localStorage.getItem('saved-count') || 0
-    );
-  });
+  const [count, setCount] = React.useState(0);
 
   React.useEffect(() => {
-    window.localStorage.setItem('saved-count', count);
+    setCount(Number(window.localStorage.getItem("saved-count") || 0));
+  }, []);
+
+  React.useEffect(() => {
+    window.localStorage.setItem("saved-count", count);
   }, [count]);
 
   return (
-    <button
-      className="count-btn"
-      onClick={() => setCount(count + 1)}
-    >
+    <button className="count-btn" onClick={() => setCount(count + 1)}>
       Count: {count}
     </button>
   );
