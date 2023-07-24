026d58c486c820713a0c2d44b8d105faf37bcb2b
Andres Sepulveda <andresdsep@yahoo.es>
2023-07-24 04:52:02 +0000

    Fix mismatch error

--- src/app/exercises/01-clock/Clock.js
+++ src/app/exercises/01-clock/Clock.js
@@ -1,9 +1,9 @@
-'use client';
-import React from 'react';
-import format from 'date-fns/format';
+"use client";
+import React from "react";
+import format from "date-fns/format";
 
 function Clock() {
-  const [time, setTime] = React.useState(new Date());
+  const [time, setTime] = React.useState();
 
   React.useEffect(() => {
     const intervalId = window.setInterval(() => {
@@ -15,9 +15,7 @@ function Clock() {
     };
   }, []);
 
-  return (
-    <p className="clock">{format(time, 'hh:mm:ss.S a')}</p>
-  );
+  return <p className="clock">{time ? format(time, "hh:mm:ss.S a") : "---"}</p>;
 }
 
 export default Clock;
