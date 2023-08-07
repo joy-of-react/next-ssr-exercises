923f4af54afa80e916e64961efcd7e7840320f9e
Andres Sepulveda <a.sepulveda@valdperformance.com>
2023-08-07 14:21:42 +1000

    Fix hydration error on interview

--- src/app/exercises/03-interview/page.js
+++ src/app/exercises/03-interview/page.js
@@ -1,35 +1,21 @@
-'use client';
-import React from 'react';
-import { useMediaQuery } from 'react-responsive';
-
-import Interview from './Interview';
-import './styles.css';
+import React from "react";
+import Interview from "./Interview";
+import "./styles.css";
 
 function InterviewExercise() {
-  const isDesktop = useMediaQuery({
-    query: '(min-width: 500px)',
-  });
-
   return (
     <main>
       <Interview />
-      {isDesktop && (
-        <aside>
-          <img
-            src="/gwen-artist.png"
-            alt="Portrait of the artist"
-          />
-          <h2>About the Artist</h2>
-          <p>
-            Gwen Altaria is a contemporary artist known for
-            her unique blend of traditional oil painting
-            techniques and pop culture references. Born and
-            raised in London, England, Vivi had an early
-            love for both the fine arts and the fantastical
-            worlds of video games.
-          </p>
-        </aside>
-      )}
+      <aside>
+        <img src="/gwen-artist.png" alt="Portrait of the artist" />
+        <h2>About the Artist</h2>
+        <p>
+          Gwen Altaria is a contemporary artist known for her unique blend of
+          traditional oil painting techniques and pop culture references. Born
+          and raised in London, England, Vivi had an early love for both the
+          fine arts and the fantastical worlds of video games.
+        </p>
+      </aside>
     </main>
   );
 }
